e2e0a4a42aaad9a4a107ed993058243c14fbc957
Richard Rosa <richard@pop-os.localdomain>
2021-12-12 01:32:03 +0000

    Criado tela de listagem de eventos e telas de modelo para <header, footer e main-page>

--- agenda_django/urls.py
+++ agenda_django/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic import RedirectView
+
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('eventos/<titulo_evento>/', views.evento),
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Evento
 
 # Create your views here.
+def evento(request, titulo_evento):
+    evento = Evento.objects.get(titulo=titulo_evento)
+    return HttpResponse(f'Descrição do evento: {evento.descricao if evento.descricao else "Sem descrição"}')
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
