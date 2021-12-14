717baec98b1d5bffb1c16c97002cba21952ef2b9
Richardflx <rfelixr@outlook.com>
2021-12-14 00:53:54 +0000

    Criado tela e função de autenticação de usuário

--- agenda_django/urls.py
+++ agenda_django/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('eventos/<titulo_evento>/', views.evento),
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Evento
 
 # Create your views here.
@@ -6,7 +9,32 @@ def evento(request, titulo_evento):
     evento = Evento.objects.get(titulo=titulo_evento)
     return HttpResponse(f'Descrição do evento: {evento.descricao if evento.descricao else "Sem descrição"}')
 
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect("/")
+        else:
+            messages.error(request, 'Usuário ou Senha inválido')
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
